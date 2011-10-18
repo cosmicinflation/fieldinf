23327383e796becc8f22043d57d4e6829a7c8f0f
chris <chris@ringeval.com>
2011-10-18 08:49:45 +0200

    sr modules and function changed in libsrmodels

--- infsric.F90
+++ infsric.F90
@@ -41,19 +41,19 @@ contains
     select case (infParam%name)
 
     case ('largef')
-       slowroll_initial_matter = lf_initial_field(infParam,efold)
+       slowroll_initial_matter = lfi_initial_field(infParam,efold)
 
     case ('smallf')
-       slowroll_initial_matter = sf_initial_field(infParam,efold)
+       slowroll_initial_matter = sfi_initial_field(infParam,efold)
 
     case ('kksf')
-       slowroll_initial_matter = kksf_initial_field(infParam,efold)
+       slowroll_initial_matter = sfbi_initial_field(infParam,efold)
 
     case ('kklt')
-       slowroll_initial_matter = kklt_initial_field(infParam,efold)
+       slowroll_initial_matter = bi_initial_field(infParam,efold)
 
     case ('mixlf')
-       slowroll_initial_matter = mixlf_initial_field(infParam,efold)
+       slowroll_initial_matter = mlfi_initial_field(infParam,efold)
 
     case default
        stop 'slowroll_initial_matter: model not implemented!'
@@ -73,10 +73,10 @@ contains
 
 
 
-  function lf_initial_field(infParam,efold)
-    use lfsrevol, only : lf_x_endinf,lf_x_trajectory
+  function lfi_initial_field(infParam,efold)
+    use lfisr, only : lfi_x_endinf,lfi_x_trajectory
     implicit none
-    real(kp), dimension(matterNum) :: lf_initial_field
+    real(kp), dimension(matterNum) :: lfi_initial_field
     type(infbgparam), intent(in) :: infParam
     real(kp), intent(in) :: efold
 
@@ -89,19 +89,19 @@ contains
 
     if (display) write(*,*)'lf_initial_field: xend= ',xEnd
 
-    xIni = lf_x_trajectory(bfold,xEnd,p)
+    xIni = lfi_x_trajectory(bfold,xEnd,p)
 
-    lf_initial_field(:) = xIni
+    lfi_initial_field(:) = xIni
 
-  end function lf_initial_field
-
-
+  end function lfi_initial_field
 
 
-  function sf_initial_field(infParam,efold)
-    use sfsrevol, only : sf_x_endinf,sf_x_trajectory
+
+
+  function sfi_initial_field(infParam,efold)
+    use sfisr, only : sfi_x_endinf,sfi_x_trajectory
     implicit none
-    real(kp), dimension(matterNum) :: sf_initial_field
+    real(kp), dimension(matterNum) :: sfi_initial_field
     type(infbgparam), intent(in) :: infParam
     real(kp), intent(in) :: efold
 
@@ -115,22 +115,22 @@ contains
     if (mu.le.0._kp) stop 'sf_initial_field: improper mu<0!'
     if (p.lt.2._kp) stop 'sf_initial_field: improper p<2!'
 
-    xEnd = sf_x_endinf(p,mu)
+    xEnd = sfi_x_endinf(p,mu)
 
     if (display) write(*,*)'sf_initial_field: xend= ',xEnd
 
-    xIni = sf_x_trajectory(bfold,xEnd,p,mu)
+    xIni = sfi_x_trajectory(bfold,xEnd,p,mu)
 
-    sf_initial_field(:) = xIni * mu
+    sfi_initial_field(:) = xIni * mu
 
-  end function sf_initial_field
+  end function sfi_initial_field
 
 
 
-  function kksf_initial_field(infParam,efold)
-    use kksfsrevol, only : kksf_x_endinf,kksf_x_trajectory
+  function sfbi_initial_field(infParam,efold)
+    use sfbisr, only : sfbi_x_endinf,sfbi_x_trajectory
     implicit none
-    real(kp), dimension(matterNum) :: kksf_initial_field
+    real(kp), dimension(matterNum) :: sfbi_initial_field
     type(infbgparam), intent(in) :: infParam
     real(kp), intent(in) :: efold
 
@@ -144,23 +144,23 @@ contains
     if (mu.le.0._kp) stop 'kksf_initial_field: improper mu<0!'
     if (p.lt.0._kp) stop 'kksf_initial_field: improper p<0!'
 
-    xEnd = kksf_x_endinf(p,mu)
+    xEnd = sfbi_x_endinf(p,mu)
 
     if (display) write(*,*)'kksf_initial_field: xend= ',xEnd
 
     xIni = kksf_x_trajectory(bfold,xEnd,p,mu)
 
-    kksf_initial_field(:) = xIni * mu
+    sfbi_initial_field(:) = xIni * mu
 
-  end function kksf_initial_field
+  end function sfbi_initial_field
   
 
 
   
-  function kklt_initial_field(infParam,efold)
-    use kkltsrevol, only : kklt_x_endinf,kklt_x_trajectory
+  function bi_initial_field(infParam,efold)
+    use bisr, only : bi_x_endinf,bi_x_trajectory
     implicit none
-    real(kp), dimension(matterNum) :: kklt_initial_field
+    real(kp), dimension(matterNum) :: bi_initial_field
     type(infbgparam), intent(in) :: infParam
     real(kp), intent(in) :: efold
 
@@ -172,10 +172,10 @@ contains
     p = infParam%consts(2)
     mu = infParam%consts(3)
 
-    if (mu.le.0._kp) stop 'kklt_initial_field: improper mu<0!'
-    if (p.lt.2._kp) stop 'kklt_initial_field: improper p<0!'
+    if (mu.le.0._kp) stop 'bi_initial_field: improper mu<0!'
+    if (p.lt.2._kp) stop 'bi_initial_field: improper p<0!'
     if (infParam%consts(5).ne.-1._kp) then
-       stop 'kklt_initial_field: improper consts(5)'
+       stop 'bi_initial_field: improper consts(5)'
     endif
 
     fieldUv = field_thbound(infParam)
@@ -184,29 +184,29 @@ contains
     xUv = fieldUv(1)/mu
     xStrg = fieldStop(1)/mu
 
-    xEps = kklt_x_endinf(p,mu)
+    xEps = bi_x_endinf(p,mu)
 
 !if phistrg occurs before slow-roll violation
     xEnd = max(xStrg,xEps)
 
-    if (display) write(*,*)'kklt_initial_field: xend= ',xEnd
+    if (display) write(*,*)'bi_initial_field: xend= ',xEnd
 
     if (xEnd.gt.xUv) then
-       write(*,*)'kklt_initial_field: xEnd > XUv!'
+       write(*,*)'bi_initial_field: xEnd > XUv!'
     endif
 
-    xIni = kklt_x_trajectory(bfold,xEnd,p,mu)
+    xIni = bi_x_trajectory(bfold,xEnd,p,mu)
 
-    kklt_initial_field(:) = xIni * mu
+    bi_initial_field(:) = xIni * mu
 
-  end function kklt_initial_field
+  end function bi_initial_field
 
 
 
-  function mixlf_initial_field(infParam,efold)
-    use mixlfsrevol, only : mixlf_x_endinf,mixlf_x_trajectory
+  function mlfi_initial_field(infParam,efold)
+    use mlfisr, only : mlfi_x_endinf,mlfi_x_trajectory
     implicit none
-    real(kp), dimension(matterNum) :: mixlf_initial_field
+    real(kp), dimension(matterNum) :: mlfi_initial_field
     type(infbgparam), intent(in) :: infParam
     real(kp), intent(in) :: efold
 
@@ -219,19 +219,19 @@ contains
     q = infParam%consts(12)
     alpha = infParam%consts(6)
 #else
-    stop 'mixlf_initial_field: -DPP12 not defined!'
+    stop 'mlfi_initial_field: -DPP12 not defined!'
 #endif
     
 
-    xEnd = mixlf_x_endinf(p,q,alpha)
+    xEnd = mlfi_x_endinf(p,q,alpha)
 
-    if (display) write(*,*)'mixlf_initial_field: xend= ',xEnd
+    if (display) write(*,*)'mlfi_initial_field: xend= ',xEnd
 
-    xIni = mixlf_x_trajectory(bfold,xEnd,p,q,alpha)
+    xIni = mlfi_x_trajectory(bfold,xEnd,p,q,alpha)
 
-    mixlf_initial_field(:) = xIni
+    mlfi_initial_field(:) = xIni
 
-  end function mixlf_initial_field
+  end function mlfi_initial_field
   
 
 
