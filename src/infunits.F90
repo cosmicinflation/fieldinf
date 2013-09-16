0686a7dae2a9b3716a51b0ccaa70bfd7fd7c3bf4
chris <chris@ringeval.com>
2013-09-16 18:13:20 +0200

    updating constants to match aspic

--- src/infunits.F90
+++ src/infunits.F90
@@ -12,7 +12,7 @@ module infunits
   
   public
 
-  real(kp), parameter :: HubbleSquareRootOf3OmegaRad = 7.4585d-63
+  real(kp), parameter :: HubbleSquareRootOf3OmegaRad = 7.5437d-63
   real(kp), parameter :: HubbleSquareRootOf2OmegaRad = sqrt(2._kp/3._kp)*HubbleSquareRootOf3OmegaRad
 
   real(kp), parameter :: lnMpcToKappa = 130.282_kp
@@ -20,11 +20,11 @@ module infunits
   real(kp), parameter :: lnMpinGeV=42.334_kp
 
 !1MeV
-!!  real(kp), parameter :: lnRhoNuc = -196.97
+!!  real(kp), parameter :: lnRhoNuc = -196.98
 !10MeV
-  real(kp), parameter :: lnRhoNuc = -187.747
+  real(kp), parameter :: lnRhoNuc = -187.77
 !100MeV
-!!  real(kp), parameter :: lnRhoNuc = -178.55
+!!  real(kp), parameter :: lnRhoNuc = -178.56
 
 #endif
 
