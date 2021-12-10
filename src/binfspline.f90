75fc4d6865a156e740732c7abd5fa312a30cc5b0
chris <chris@ringeval.com>
2021-12-10 16:27:57 +0100

    Update headers

--- src/binfspline.f90
+++ src/binfspline.f90
@@ -1,3 +1,28 @@
+!   This file is part of FieldInf
+!
+!   Copyright (C) 2005-2021 C. Ringeval (minor changes)
+!
+!   Copyright (C) 2000 Wolfgang Schadow (see below for the original
+!   licensing)
+!   
+!   FieldInf is free software: you can redistribute it and/or modify
+!   it under the terms of the GNU General Public License as published by
+!   the Free Software Foundation, either version 3 of the License, or
+!   (at your option) any later version.
+!
+!   FieldInf is distributed in the hope that it will be useful,
+!   but WITHOUT ANY WARRANTY; without even the implied warranty of
+!   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!   GNU General Public License for more details.
+!
+!   You should have received a copy of the GNU General Public License
+!   along with FieldInf.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
+
+
+
 ! %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 !
 !
